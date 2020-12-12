64265f3fac9ef4f9a216aa5e58f924138aa690ee
KimNikita <ghj710@mail.ru>
2020-12-12 21:36:13 +0300

    Begin

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build Status](https://travis-ci.org/KimNikita/Lab_7.svg?branch=main)](https://travis-ci.org/KimNikita/Lab_7)
+
 # Lab_7
---
+++ main/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "ArrayList.h"
+
+using namespace std;
+
+int main()
+{
+  TArrayList<int> a;
+  cout << a;
+  return 0;
+}
